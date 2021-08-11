c139722127ef70e36201321b86fdce6bed359e90
11akanksha <64818557+11akanksha@users.noreply.github.com>
2021-08-11 23:08:47 +0530

    open cv
    3 videos in one

---
+++ Filter.py
@@ -0,0 +1,34 @@
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+
+    if ret == False:
+        continue
+
+    cv2.imshow("Video Frame", frame)
+
+    key_pressed = cv2.waitKey(1) & 0xff
+    if key_pressed == ord('q'):
+        break
+
+while True:
+    ret, frame = cap.read()
+    grey_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    cv2.imshow("Gray Frame", grey_frame)
+    key_pressed = cv2.waitKey(1) & 0xff
+    if key_pressed == ord('q'):
+        break
+
+while True:
+    ret, frame = cap.read()
+    rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    cv2.imshow("RGB Frame", rgb_frame)
+    key_pressed = cv2.waitKey(1) & 0xff
+    if key_pressed == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
